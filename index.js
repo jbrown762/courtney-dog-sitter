3192e4bb649f91ad46d0a877139e35af3b9ae962
Joe Brown <jbrown762@gmail.com>
2017-06-27 19:11:04 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+//var logger = function(req, res, next){
+//    console.log('Logging...');
+//    next();
+//}
+//
+//app.use(logger);
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//app.get('/', function (req, res) {
+//
+//});
+
+app.listen(3000, function () {
+    console.log('Server started on Port 3000...');
+})
